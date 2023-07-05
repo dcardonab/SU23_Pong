8f4d104af27801943a7eafcec2d47e86d1042b01
dcardonab <dcbapc@gmail.com>
2023-07-05 07:43:07 -0400

    Added Singleton pattern comment

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -8,7 +8,24 @@ public class GameBehavior : MonoBehaviour
 
     void Awake()
     {
-        // Singleton Pattern
+        /*
+         * Singleton Pattern
+         *
+         * The reference to the script is static, meaning that it is the same
+         * in any and all instances of the class. However, the instance that
+         * the reference points to will not necessarily be the same.
+         * 
+         * This means that although the reference can only point to one
+         * instance of the script, it is possible to have multiple instances
+         * of a singleton in the scene.
+         * 
+         * As such, it is important to ensure that only one instance of a
+         * singleton exists. This is done by checking to see if the static
+         * reference matches the script instance. If it does not, it will be
+         * a duplication and will delete itself.
+         * 
+         * The conditional below is known as the singleton pattern.
+         */
         if (Instance != this && Instance != null)
             Destroy(this);
         else
